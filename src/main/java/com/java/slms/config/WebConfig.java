80caad47a31077afc1a6bd6c77384e59532da2ff
Gourav Dodia <gouravkumardodia@gmail.com>
2026-04-11 12:12:53 +0530

    changes

--- src/main/java/com/java/slms/config/WebConfig.java
+++ src/main/java/com/java/slms/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig  implements WebMvcConfigurer
 	public void addCorsMappings(CorsRegistry registry) {
 	    registry.addMapping("/**")
 	            .allowedOriginPatterns("http://3.110.94.0","http://classconnect.it.com")
-	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	            .allowedMethods("GET", "POST", "PUT","PATCH" ,"DELETE", "OPTIONS")
 	            .allowedHeaders("*")
 	            .exposedHeaders("Authorization")
 	            .allowCredentials(true)
